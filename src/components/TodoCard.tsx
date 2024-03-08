fd79198b31b7e8315fef49333fe6abd867fed1e0
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-03-08 19:23:41 +0545

    useMutation implemented for deleting todo

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,8 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { TGetAllTodosOutput } from "../type";
 import styles from "./TodoCard.module.css";
+import { MdDelete, MdEdit } from "react-icons/md";
 
 export function TodoCard() {
+  const qc = useQueryClient();
+
   const { data, isLoading, isError, error } = useQuery<
     TGetAllTodosOutput,
     Error
@@ -20,6 +23,33 @@ export function TodoCard() {
     },
   });
 
+  const deleteTodoMutation = useMutation({
+    mutationFn: async (todoId: string) => {
+      const res = await fetch(`http://localhost:8080/api/v1/todos/${todoId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      return data;
+    },
+    onSuccess: (data) => {
+      console.log(data);
+      qc.invalidateQueries({
+        queryKey: ["/api/v1/todos"],
+      });
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  const handleDeleteTodoBtnClick = async (todoId: string) => {
+    console.log("handleDeleteTodoBtnClick function called", todoId);
+    await deleteTodoMutation.mutateAsync(todoId);
+  };
+
   if (isLoading) return <p>Loading todos...</p>;
   if (isError) return <p>Loading todos errors: {error.message || ""}</p>;
 
@@ -28,8 +58,20 @@ export function TodoCard() {
       {data?.data.map((todo) => {
         return (
           <div key={todo._id} className={styles.card}>
-            <h2>Tittle: {todo.title}</h2>
-            <p>Description: {todo.description}</p>
+            <div className={styles.buttons}>
+              <MdEdit className={styles.button} />
+              <MdDelete
+                className={styles.button}
+                onClick={() => {
+                  console.log("Delete button clicked");
+                  handleDeleteTodoBtnClick(todo._id);
+                }}
+              />
+            </div>
+            <div className={styles.content}>
+              <h2>Title: {todo.title}</h2>
+              <p>Description: {todo.description}</p>
+            </div>
           </div>
         );
       })}
